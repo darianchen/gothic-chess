73bc72a7db81e3f83ea9d72d3b15617524fc0935
Brian <brianrlam@gmail.com>
2023-02-27 14:27:42 -0500

    render test board

--- src/components/Board/index.jsx
+++ src/components/Board/index.jsx
@@ -1,27 +1,47 @@
-
+import './index.css';
+import whiterook from '../../assets/whiterook.png';
+import blackknight from '../../assets/blackknight.png';
 
 function Board (){
-let board = [];
-const column = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
-const row = [1, 2, 3, 4, 5, 6, 7, 8];
 
-const renderBoard = () => {
-    for(let i = row.length - 1; i >= 0; i--){
-        let tempRow = []
-        for(let j = 0; j < column.length - 1; j++){
-            tempRow.push(`${column[j]}${column[i]}`);
-        }
-        board.push(tempRow);
-    }
-    return board
-}
+    const rook = {icon: whiterook};
+    const knight = {icon: blackknight}
+    const testBoard = [
+        [null,null,null,null,null,null,null,null,null,null,],
+        [null,rook,null,null,rook,null,null,null,null,null,],
+        [null,null,null,null,null,null,null,null,null,null,],
+        [null,null,null,null,null,null,null,null,null,null,],
+        [null,null,null,null,null,null,null,null,null,null,],
+        [null,null,knight,null,null,null,null,knight,null,null,],
+        [null,null,null,null,null,null,null,null,null,null,],
+        [null,null,null,null,null,null,null,null,null,null,],
+    ];
 
-renderBoard()
-return(
-    <div>
-        {board}
-    </div>
-)
+    const column = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
+    const row = [1, 2, 3, 4, 5, 6, 7, 8];
+    const colors = ['white','black'];
+
+    let theBoard = testBoard;
+
+
+    console.log(rook.icon)
+    return(
+        <>
+            <h1>This is the Board</h1>
+            <div>
+                {theBoard.map((theRow,rowIdx)=>{
+                    return <div className="row" key={rowIdx}>
+                        {theRow.map( (theCol,colIdx)=>{
+                            return <div className="tile" id={colors[(rowIdx+colIdx)%2]} key={colIdx}>
+                                {theBoard[rowIdx][colIdx] ? <img src={theBoard[rowIdx][colIdx].icon}></img> : ''}
+                            </div>
+                        })}
+                    </div>
+                })}
+            </div>
+        </>
+    )
+
 }
 
 
