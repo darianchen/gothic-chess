f78f83228eabb00f91bec5f4fc6ed34fe9dd6c75
Darian <darianychen@gmail.com>
2023-02-28 18:56:58 -0500

    Implement bishop class

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import Board from './components/Board';
 import Rook from './classes/Rook';
 import Knight from './classes/Knight';
+import Bishop from './classes/Bishop';
 
 function App() {
   const board = new Array(8).fill().map(()=> new Array(10).fill(null));
@@ -10,6 +11,7 @@ function App() {
     new Rook('white',board,[7,0]);
     new Rook('white',board,[7,9]);
     new Knight('black',board,[0,1]);
+    new Bishop('black', board,[0,3]);
   }
 
   startGame();
---
+++ src/classes/Bishop.js
@@ -0,0 +1,44 @@
+import Piece from "./Piece";
+import whiteBishop from '../assets/Images/white_bishop.png';
+import blackBishop from '../assets/Images/black_bishop.png';
+
+class Bishop extends Piece {
+    constructor(color,board,position){
+        super(color,board,position);
+        if(color==='white'){
+            this.image = whiteBishop;
+        } else {
+            this.image = blackBishop;
+        }
+    }
+
+    availableMoves() {
+        const [row, col] = this.position;
+        const moves = [];
+
+        const dirs = [[1,1],[1,-1],[-1,1],[-1,-1]];
+
+        const check = (r,c,dir) => {
+            const [newRow,newCol] = [r+dir[0],c+dir[1]];
+            if(this.outOfBounds(newRow,newCol)) return;
+            if(this.board[newRow][newCol]){
+                if(this.board[newRow][newCol].color !== this.color) moves.push([newRow,newCol]);
+                return;
+            } else {
+                moves.push([newRow,newCol]);
+                check(newRow,newCol,dir);
+            }
+        };
+
+        for(let dir of dirs) check(row,col,dir);
+
+        return moves;
+    }
+
+    canMove(newPos) {
+        const moves = this.availableMoves();
+        return moves.some(move => move[0] === newPos[0] && move[1] === newPos[1]);
+    }
+}
+
+export default Bishop;
