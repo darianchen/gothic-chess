544a4efd3b85e148b41b28f8ba3e78d2561f6c55
Darian <darianychen@gmail.com>
2023-02-28 16:29:45 -0500

    Implement functions in rook class and add images

---
+++ src/classes/Knight.js
@@ -0,0 +1,9 @@
+import Piece from "./Piece";
+import whiterook from '../assets/Images/whiterook.png';
+import blackknight from '../assets/Images/blackknight.png';
+
+class Knight extends Piece {
+
+}
+
+export default Rook;
--- src/classes/Rook.js
+++ src/classes/Rook.js
@@ -1,6 +1,6 @@
 import Piece from "./Piece";
-import whiterook from '../assets/whiterook.png';
-import blackknight from '../assets/blackknight.png';
+import whiterook from '../assets/Images/white_rook.png';
+import blackrook from '../assets/Images/black_rook.png';
 
 class Rook extends Piece {
     constructor(color,board,position){
@@ -8,18 +8,72 @@ class Rook extends Piece {
         if(color==='white'){
             this.image = whiterook;
         } else {
-            // this is for testing, darian please fix
-            this.image = blackknight;
+            this.image = blackrook;
         }
     }
 
-    availableMoves(){
-        // returns an array of squares this piece can move to
+    availableMoves() {
+        const [row, col] = this.position;
+        const moves = [];
+
+        // Check moves to the top of the board
+        const checkUp = (row, col) => {
+            if (row < 0) return;
+            const piece = this.board[row][col];
+            if (!piece || piece.color !== this.color) {
+                moves.push([row, col]);
+            }
+            if (!piece) {
+                checkUp(row - 1, col);
+            }
+        };
+        checkUp(row - 1, col);
+
+        // Check moves to the bottom of the board
+        const checkDown = (row, col) => {
+            if (row >= this.board.length) return;
+            const piece = this.board[row][col];
+            if (!piece || piece.color !== this.color) {
+                moves.push([row, col]);
+            }
+            if (!piece) {
+                checkDown(row + 1, col);
+            }
+        };
+        checkDown(row + 1, col);
+
+        // Check moves to the left of the board
+        const checkLeft = (row, col) => {
+            if (col < 0) return;
+            const piece = this.board[row][col];
+            if (!piece || piece.color !== this.color) {
+                moves.push([row, col]);
+            }
+            if (!piece) {
+                checkLeft(row, col - 1);
+            }
+        };
+        checkLeft(row, col - 1);
+
+        // Check moves to the right of the board
+        const checkRight = (row, col) => {
+            if (col >= this.board[0].length) return;
+            const piece = this.board[row][col];
+            if (!piece || piece.color !== this.color) {
+                moves.push([row, col]);
+            }
+            if (!piece) {
+                checkRight(row, col + 1);
+            }
+        };
+        checkRight(row, col + 1);
+
+        return moves;
     }
 
-    canMove(newPos){
-        // returns boolean whether this piece can move to new position
-        return true;
+    canMove(newPos) {
+        const moves = this.availableMoves();
+        return moves.some(move => move[0] === newPos[0] && move[1] === newPos[1]);
     }
 }
 
