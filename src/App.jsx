c05d43f79ce2dc88974c7bd89ad9e2da97afec62
Darian <darianychen@gmail.com>
2023-02-28 21:19:00 -0500

    Add queen and implement more of board

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Knight from './classes/Knight';
 import Bishop from './classes/Bishop';
 import Princess from './classes/Princess';
 import Empress from './classes/Empress';
+import Queen from './classes/Queen';
 
 function App() {
   const board = new Array(8).fill().map(()=> new Array(10).fill(null));
@@ -13,11 +14,21 @@ function App() {
     new Rook('black',board,[0,0]);
     new Rook('black',board,[0,9]);
     new Knight('black',board,[0,1]);
+    new Knight('black',board,[0,8]);
     new Bishop('black', board,[0,2]);
+    new Bishop('black', board,[0,7]);
+    new Queen('white',board,[7,3]);
+    new Queen('black',board,[0,3]);
+    new Empress('black', board,[0,4]);
+    new Princess('black',board,[0,6]);
     new Empress('white', board,[7,4]);
     new Princess('white',board,[7,6]);
     new Rook('white',board,[7,0]);
     new Rook('white',board,[7,9]);
+    new Knight('white',board,[7,1]);
+    new Knight('white',board,[7,8]);
+    new Bishop('white',board,[7,2]);
+    new Bishop('white',board,[7,7]);
   }
 
   startGame();
---
+++ src/classes/Queen.js
@@ -0,0 +1,39 @@
+import Piece from "./Piece";
+import whiteQueen from '../assets/Images/white_queen.png';
+import blackQueen from '../assets/Images/black_queen.png';
+
+class Queen extends Piece {
+    constructor(color,board,position){
+        super(color,board,position);
+        if(color==='white'){
+            this.image = whiteQueen;
+        } else {
+            this.image = blackQueen;
+        }
+    }
+
+    availableMoves() {
+        const [row, col] = this.position;
+        const moves = [];
+
+        const dirs = [[0,1],[0,-1],[1,0],[-1,0],[1,1],[1,-1],[-1,1],[-1,-1]];
+
+        const check = (r,c,dir) => {
+            const [newRow,newCol] = [r+dir[0],c+dir[1]];
+            if(this.outOfBounds(newRow,newCol)) return;
+            if(this.board[newRow][newCol]){
+                if(this.board[newRow][newCol].color !== this.color) moves.push([newRow,newCol]);
+                return;
+            } else {
+                moves.push([newRow,newCol]);
+                check(newRow,newCol,dir);
+            }
+        };
+
+        for(let dir of dirs) check(row,col,dir);
+
+        return moves;
+    }
+}
+
+export default Queen;
