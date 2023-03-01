9932693e8556421f1ced8ec77d90644d71c353c0
Brian <brianrlam@gmail.com>
2023-03-01 11:52:16 -0500

    do basic pawn moves

--- src/classes/Pawn.js
+++ src/classes/Pawn.js
@@ -22,9 +22,15 @@ class Pawn extends Piece {
         const [row, col] = this.position;
         const moves = [];
         const forwardDir = this.color === "white" ? -1 : 1; // the direction in which the pawn moves forward
-        const startRow = this.color === "white" ? 6 : 1; // the row on which the pawn starts
-        const newRow = row + forwardDir;
-        const doubleNextRow = row + 2 * forwardDir;
+        // const startRow = this.color === "white" ? 6 : 1; // the row on which the pawn starts
+        // const newRow = row + forwardDir;
+        // const doubleNextRow = row + 2 * forwardDir;
+        if(!this.board[row+forwardDir][col]) moves.push([row+forwardDir,col]);
+        if(!this.hasMoved && !this.board[row+2*forwardDir][col]) moves.push([row+2*forwardDir,col]);
+        if(this.board[row+forwardDir][col+1]&&this.board[row+forwardDir][col+1].color!==this.color) moves.push([row+forwardDir,col+1]);
+        if(this.board[row+forwardDir][col-1]&&this.board[row+forwardDir][col-1].color!==this.color) moves.push([row+forwardDir,col-1]);
+
+        return moves;
     }
 }
 
