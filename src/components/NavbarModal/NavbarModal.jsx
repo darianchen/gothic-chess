46272086802329c3fcec34c1ef1785b9fe986ebb
Brian <brianrlam@gmail.com>
2023-03-20 14:36:25 -0400

    update howtoplay icons

--- src/components/NavbarModal/NavbarModal.jsx
+++ src/components/NavbarModal/NavbarModal.jsx
@@ -1,5 +1,8 @@
-import Princesses from "../../assets/Images/princesses.png";
-import Empresses from "../../assets/Images/empresses.png";
+
+import white_princess from "../../assets/Images/white_princess.svg"
+import black_princess from "../../assets/Images/black_princess.svg"
+import white_empress from "../../assets/Images/white_empress.svg"
+import black_empress from "../../assets/Images/black_empress.svg"
 
 function NavbarModal({setOpenModal, modalType}) {
     switch (modalType) {
@@ -30,8 +33,11 @@ function NavbarModal({setOpenModal, modalType}) {
                                 Gothic Chess adds two new pieces to the game: the Princess and the Empress. The Empress moves like a Bishop and a Knight combined, and the Chancellor moves like a Rook and a Knight combined.
                                 </p>
                                 <div id="new-piece-container">
-                                    <img src={Princesses} alt="Princess"/>
-                                    <img src={Empresses} alt="Empress"/>
+                                    <img src={white_princess} width={100} height={100} alt=''/>
+                                    <img src={black_princess} width={100} height={100} alt=''/>
+                                    <img src={white_empress} width={100} height={100} alt=''/>
+                                    <img src={black_empress} width={100} height={100} alt=''/>
+
                                 </div>
                                 <p>The game also has a different castling rule. In Gothic Chess, the King moves three squares in either direction, and the Rook moves to the square adjacent to the King.</p>
                             </div>
